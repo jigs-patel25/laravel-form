13aae4ef2ba8011bf222804388c7a3a533e0ead0
kishanranoliya <kishan@wavel.ai>
2024-06-28 14:45:27 +0530

    change form

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <div class="container">\
+        <div class="row">
+
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="" class="form-label">FILE</label>
+                    <input type="text" name=""
+                    555
+                     id="" class="form-control" placeholder="" aria-describedby="helpId" />
+                    <small id="helpId" class="text-muted">Help text</small>
+                </div>
+            </form>
+
+            </div>
+        </div>
+
+</body>
+</html>
